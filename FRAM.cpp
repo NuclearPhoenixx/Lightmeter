5a76e55b94d0b485fb36dfd6fb64c5dac8b1b4c3
Phoenix1747 <apertured@public-files.de>
2018-07-02 12:56:45 +0200

    misc update

---
+++ FRAM.cpp
@@ -0,0 +1,248 @@
+/*
+ * Original Author: KTOWN (Adafruit Industries), licensed under BSD - see header file!
+ * Changes involved to make it work best with my lightmeter.
+*/
+#include <stdlib.h>
+#include <math.h>
+
+#include "FRAM.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+
+FRAM_SPI::FRAM_SPI(int8_t cs)
+{
+  _cs = cs;
+  _framInitialised = false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Initializes SPI and configures the chipdoing anything else)
+
+boolean FRAM_SPI::begin()
+{
+  return begin(_cs, 2);
+}
+
+boolean FRAM_SPI::begin(uint8_t nAddressSizeBytes)
+{
+  return begin(_cs, nAddressSizeBytes);
+}
+
+boolean FRAM_SPI::begin(int8_t cs, uint8_t nAddressSizeBytes)
+{
+  _cs = cs;
+  setAddressSize(nAddressSizeBytes);
+
+  /* Configure SPI */
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
+
+  SPI.begin();
+  SPI.setClockDivider (SPI_CLOCK_DIV2); // 8 MHz
+  SPI.setDataMode(SPI_MODE0);
+
+  /* Make sure we're actually connected */
+  uint8_t manufID;
+  uint16_t prodID;
+  getDeviceID(&manufID, &prodID);
+
+  if (manufID != 0x04 && manufID != 0x7f)
+  {
+    //Serial.print("Unexpected Manufacturer ID: 0x"); Serial.println(manufID, HEX);
+    return false;
+  }
+  if (prodID != 0x0302 && prodID != 0x7f7f)
+  {
+    //Serial.print("Unexpected Product ID: 0x"); Serial.println(prodID, HEX);
+    return false;
+  }
+
+  /* Everything seems to be properly initialised and connected */
+  _framInitialised = true;
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Enables or disables writing to the SPI flash
+
+void FRAM_SPI::writeEnable (bool enable)
+{
+  digitalWrite(_cs, LOW);
+  if (enable)
+  {
+    SPItransfer(OPCODE_WREN);
+  }
+  else
+  {
+    SPItransfer(OPCODE_WRDI);
+  }
+  digitalWrite(_cs, HIGH);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Writes a byte at the specific FRAM address
+
+    @params[in] addr
+                The 32-bit address to write to in FRAM memory
+    @params[in] i2cAddr
+                The 8-bit value to write at framAddr
+*/
+/**************************************************************************/
+void FRAM_SPI::write8 (uint32_t addr, uint8_t value)
+{
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_WRITE);
+  writeAddress(addr);
+  SPItransfer(value);
+  /* CS on the rising edge commits the WRITE */
+  digitalWrite(_cs, HIGH);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Writes count bytes starting at the specific FRAM address
+
+    @params[in] addr
+                The 32-bit address to write to in FRAM memory
+    @params[in] values
+                The pointer to an array of 8-bit values to write starting at addr
+    @params[in] count
+                The number of bytes to write
+*/
+/**************************************************************************/
+void FRAM_SPI::write (uint32_t addr, const uint8_t *values, size_t count)
+{
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_WRITE);
+  writeAddress(addr);
+  for (size_t i = 0; i < count; i++)
+  {
+    SPItransfer(values[i]);
+  }
+  /* CS on the rising edge commits the WRITE */
+  digitalWrite(_cs, HIGH);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads an 8 bit value from the specified FRAM address
+
+    @params[in] addr
+                The 32-bit address to read from in FRAM memory
+
+    @returns    The 8-bit value retrieved at framAddr
+*/
+/**************************************************************************/
+uint8_t FRAM_SPI::read8 (uint32_t addr)
+{
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_READ);
+  writeAddress(addr);
+  uint8_t x = SPItransfer(0);
+  digitalWrite(_cs, HIGH);
+  return x;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Read count bytes starting at the specific FRAM address
+
+    @params[in] addr
+                The 32-bit address to write to in FRAM memory
+    @params[out] values
+                The pointer to an array of 8-bit values to read starting at addr
+    @params[in] count
+                The number of bytes to read
+*/
+/**************************************************************************/
+void FRAM_SPI::read (uint32_t addr, uint8_t *values, size_t count)
+{
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_READ);
+  writeAddress(addr);
+  for (size_t i = 0; i < count; i++)
+  {
+    uint8_t x = SPItransfer(0);
+    values[i] = x;
+  }
+  digitalWrite(_cs, HIGH);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads the Manufacturer ID and the Product ID from the IC
+
+    @params[out]  manufacturerID
+                  The 8-bit manufacturer ID (Fujitsu = 0x04)
+    @params[out]  productID
+                  The memory density (bytes 15..8) and proprietary
+                  Product ID fields (bytes 7..0). Should be 0x0302 for
+                  the MB85RS64VPNF-G-JNERE1.
+*/
+/**************************************************************************/
+void FRAM_SPI::getDeviceID(uint8_t *manufacturerID, uint16_t *productID)
+{
+  uint8_t a[4] = { 0, 0, 0, 0 };
+  //uint8_t results;
+
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_RDID);
+  a[0] = SPItransfer(0);
+  a[1] = SPItransfer(0);
+  a[2] = SPItransfer(0);
+  a[3] = SPItransfer(0);
+  digitalWrite(_cs, HIGH);
+
+  /* Shift values to separate manuf and prod IDs */
+  /* See p.10 of http://www.fujitsu.com/downloads/MICRO/fsa/pdf/products/memory/fram/MB85RS64V-DS501-00015-4v0-E.pdf */
+  *manufacturerID = (a[0]);
+  *productID = (a[2] << 8) + a[3];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Reads the status register
+
+uint8_t FRAM_SPI::getStatusRegister()
+{
+  uint8_t reg = 0;
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_RDSR);
+  reg = SPItransfer(0);
+  digitalWrite(_cs, HIGH);
+  return reg;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Sets the status register
+
+void FRAM_SPI::setStatusRegister(uint8_t value)
+{
+  digitalWrite(_cs, LOW);
+  SPItransfer(OPCODE_WRSR);
+  SPItransfer(value);
+  digitalWrite(_cs, HIGH);
+}
+
+void FRAM_SPI::setAddressSize(uint8_t nAddressSize)
+{
+  _nAddressSizeBytes = nAddressSize;
+}
+
+uint8_t FRAM_SPI::SPItransfer(uint8_t x)
+{
+  return SPI.transfer(x);
+}
+
+void FRAM_SPI::writeAddress(uint32_t addr)
+{
+  if (_nAddressSizeBytes>3)
+    SPItransfer((uint8_t)(addr >> 24));
+  if (_nAddressSizeBytes>2)
+    SPItransfer((uint8_t)(addr >> 16));
+  SPItransfer((uint8_t)(addr >> 8));
+  SPItransfer((uint8_t)(addr & 0xFF));
+}
+
---
+++ lightmeter.ino
@@ -0,0 +1,194 @@
+/*
+  All-In-One Arduino Lightmeter
+
+  TO DO:
+  * Check what happens if the SD card space is full.
+  * Option to use date (daily) as file name.
+  * Open up settings in settings file (JSON) so that you don't need to reflash
+      the whole thing if you want to change anything.
+  * Add option to set an upper lux limit for the lightmeter to reduce the amount of data
+  * Check if uSD card is plugged in before write with CD pin.
+  * Finally implement FRAM buffering.
+*/
+#include <SD.h> //SD Card
+#include <ArduinoJson.h> //For JSON data formatting
+
+#include "RTC.h" //My DS3231 stuff
+#include "LIGHTSENSOR.h" //My TSL2591 stuff
+#include "FRAM.h" //All the FRAM stuff
+#include "EXTRA.h" //All the extra functions
+
+#define _MAJORV 4 //major firmware version
+#define _MINORV 0 //minor firmware version
+#define SD_PIN 4 //pin connected to the CS pin of the uSD card
+#define FRAM_PIN 14 //pin connected to the CS pin of the FRAM chip
+
+/* BEGIN USER CONFIG */
+const String FILE_NAME = "data"; //filename for the data file; 8 chars or less!
+const String FILE_EXTENSION = "txt"; //file extension for the data file; 3 chars or less!
+const uint32_t MAX_FILESIZE = 500000000; //max filesize in byte, here it's 500MB (NOTE FAT32 SIZE LIMIT!)
+const uint16_t M_INTERVAL = 5000; //time between measurements, in ms
+const byte MAX_TRIES = 5; //max number of re-tries after an invalid measurement before just continuing
+const byte DATA_BUFFER = 10; //how many data points get buffered before written to SD -> saves MUCH power!
+/* END USER CONFIG */
+
+
+TSL2591 lightsensor = TSL2591(1, 2); //create the objects for my classes
+RTC_DS3231 rtc;
+FRAM_SPI fram = FRAM_SPI(FRAM_PIN);
+
+File dataFile; //global variable that will hold the data file
+byte fileNum = 0; //global number of file
+String filePath; //this will hold the file path globally
+//byte lastAddrByte = 0; //this will hold the last address byte that has been written to FRAM
+//byte bufferCounter = 0; //global number of buffered data points
+
+/* ARDUINO SETUP FUNCTION */
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT); //set builtin LED to output
+
+  /* Display some basic information on this sensor
+  lightsensor.displaySensorDetails(); */
+
+  // INIT TSL2591 IF PRESENT
+  if(!lightsensor.begin())
+  {
+    while(1)
+    {
+      extra::signal_led(1);
+    }
+  }
+
+  // INIT SD CARD IF PRESENT
+  if(!SD.begin(SD_PIN))
+  {
+    while(1)
+    {
+      extra::signal_led(2);
+    }
+  }
+
+  //INIT RTC
+  if(!rtc.begin())
+  {
+    while(1)
+    {
+      extra::signal_led(5);
+    }
+  }
+  
+  //CHECK IF RTC LOST POWER AND TIME IS INVALID
+  if(rtc.lostPower())
+  {
+    while(1)
+    {
+      extra::signal_led(7);
+    }
+  }
+
+  //INIT FRAM
+  if(!fram.begin())
+  {
+    while(1)
+    {
+      extra::signal_led(8);
+    }
+  }
+  
+  //update filePath to point to file_name.file_extenion.
+  filePath = FILE_NAME + "." + FILE_EXTENSION;
+  dataFile = SD.open(filePath, FILE_WRITE); // open the data file, only one file at a time!
+
+  //grab the last saved data point address from FRAM
+  //lastAddrByte = fram.read8(lastAddrByte);
+  
+  // FIRMWARE POWERUP LED FLASH
+  for(byte x = 0; x < _MAJORV; x++) //flash major version
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+  }
+  
+  delay(400); //400ms delay between the stages
+  
+  for(byte x = 0; x < _MINORV; x++) //flash minor version
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+  }
+  
+  extra::sleep(1000); //1000ms sleep delay between possible coming errors and firmware flash
+  //Serial.begin(9600); //DEBUGGING SERIAL
+}
+
+/* MAIN LOOP */
+void loop()
+{
+  float lux = lightsensor.luxRead(); //get lux values
+  byte tries = 0; //counter for tries
+
+  while(lux <= 0) //check if lux value is valid
+  {
+    if(tries >= MAX_TRIES)
+    {
+      lux = 0.0; //set lux to 0 to signal a problem
+      break;
+    }
+    extra::sleep(600); //sleep for 600ms (worst case to let the lightsensor calm down)
+    lux = lightsensor.luxRead();
+    tries++;
+  }
+
+  //DateTime now = rtc.now();
+  uint32_t timestamp = rtc.now().unixtime(); //get unix timestamp
+
+  // buffer data, else write everything to uSD card
+  /*
+  if(bufferCounter <= DATA_BUFFER){
+    bufferCounter++;
+
+      T O D O
+    
+  }
+  */
+  
+  // new static (faster than dynamic) json document and allocate 40 bytes (worst case)
+  StaticJsonDocument<40> jsonDoc;
+  
+  // create new json object that will contain all the logged data
+  JsonObject data = jsonDoc.to<JsonObject>();
+
+  data[F("unixtime")] = timestamp; //input current RTC unixtime
+  data[F("lux")] = lux; //input lux value
+  
+  //to-be/future size of file with the new data in bytes
+  uint32_t future_size = dataFile.size() + measureJson(data);
+  
+  //check if future_size is bigger than specified max size, iterate until a valid file is opened
+  while(future_size > MAX_FILESIZE)
+  {
+    fileNum++; //add 1 to the file number
+    filePath = FILE_NAME + fileNum + "." + FILE_EXTENSION; //update filename to include the file number
+    dataFile = SD.open(filePath, FILE_WRITE);
+    future_size = dataFile.size() + measureJson(data);
+  }
+
+  //if the data file is available, write the data to it
+  if(dataFile)
+  {
+    serializeJson(data, dataFile); //append the compact data to the file
+    dataFile.flush(); //save the data to the file, needs up to 3x the power
+  }
+  else //if the file is not available, flash an error
+  {
+    extra::signal_led(3);
+  }
+
+  extra::sleep(M_INTERVAL); //enable sleep mode for the time interval between measurements
+}
+
